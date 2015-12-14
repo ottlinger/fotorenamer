51c44ca24e9eb8c5c14d5a2cb9ccc0db529122df
Hugo Hirsch <github@hugo-hirsch.de>
2015-12-15 00:16:22 +0100

    Bugfix: null if directory without image is selected.

--- src/main/java/de/aikiit/fotorenamer/exception/NoFilesFoundException.java
+++ src/main/java/de/aikiit/fotorenamer/exception/NoFilesFoundException.java
@@ -1,18 +1,18 @@
 /**
-Copyright 2011, Aiki IT, FotoRenamer
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright 2011, Aiki IT, FotoRenamer
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.aikiit.fotorenamer.exception;
 
 import org.apache.log4j.Logger;
@@ -34,12 +34,12 @@ public class NoFilesFoundException extends Exception {
             Logger.getLogger(NoFilesFoundException.class);
 
     /**
-     * Provide error messages for one directory.
+     * Provide error messages for a directory.
      *
      * @param directory Current directory.
      */
     public NoFilesFoundException(final File directory) {
-        super();
+        super(directory == null ? "none" : directory.getAbsolutePath());
         LOG.error("no files found in " + directory);
     }
 
