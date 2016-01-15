ec40d312abaf762a2b9b27ba0de4cf7a882467d3
Hugo Hirsch <github@hugo-hirsch.de>
2016-01-15 21:11:33 +0100

    Issue #10: More l10n.

--- src/main/java/de/aikiit/fotorenamer/image/RemoveExifPrefixRenamer.java
+++ src/main/java/de/aikiit/fotorenamer/image/RemoveExifPrefixRenamer.java
@@ -1,18 +1,18 @@
 /**
-Copyright 2011, Aiki IT, FotoRenamer
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright 2011, Aiki IT, FotoRenamer
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.aikiit.fotorenamer.image;
 
 import de.aikiit.fotorenamer.exception.InvalidDirectoryException;
@@ -92,8 +92,8 @@ public class RemoveExifPrefixRenamer implements Runnable {
                     new File(listOfFile.getParent()
                             + File.separatorChar + nameNeu))) {
                 LOG.error("Problem with file " + listOfFile.getName());
-                throw new RenamingErrorException("\nFehler bei Bild "
-                        + listOfFile.getName());
+                throw new RenamingErrorException(getParameterizedBundleString("fotorenamer.ui.rerename.error.detail",
+                        listOfFile.getName()));
             }
         }
     }
