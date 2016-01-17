0b372801d516b1477af25551b50aef88dea1ac2a
Hugo Hirsch <github@hugo-hirsch.de>
2016-01-17 21:53:40 +0100

    Issue #10: Add localization fixes and possible NPE bug

--- src/main/java/de/aikiit/fotorenamer/image/ImageFilenameFilter.java
+++ src/main/java/de/aikiit/fotorenamer/image/ImageFilenameFilter.java
@@ -1,19 +1,21 @@
 /**
-Copyright 2011, Aiki IT, FotoRenamer
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright 2011, Aiki IT, FotoRenamer
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.aikiit.fotorenamer.image;
+
+import com.google.common.base.Strings;
 
 import java.io.File;
 import java.io.FilenameFilter;
@@ -40,6 +42,6 @@ class ImageFilenameFilter implements FilenameFilter {
      *         the file is a JPG-picture.
      */
     public final boolean accept(final File dir, final String name) {
-        return new File(dir, name).isFile() && name.toLowerCase().endsWith(JPG);
+        return !(Strings.isNullOrEmpty(name) || dir == null) && new File(dir, name).isFile() && name.toLowerCase().endsWith(JPG);
     }
 }
