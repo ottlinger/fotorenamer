e6a71af894f3b4856c81805735f45760686b7afb
Hugo Hirsch <github@hugo-hirsch.de>
2016-04-17 22:45:33 +0200

    Change output.

--- site.sh
+++ site.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Generating new mvn site ..."
+echo "Generating new fotorenamer 'mvn site' ..."
 mvn clean site:site
 echo "DONE - ready to commit and push"
 git checkout gh-pages && ./copy-site.sh 
