f3485f715d97bfcc75f6860676636b29844b955a
Hugo Hirsch <github@hugo-hirsch.de>
2016-12-10 00:12:27 +0100

    Issue #9: Change test assertions to match files
    * Fallback to proper file suffixes instead of exact names since
    tests run before may have touched the image as well.

--- src/test/java/de/aikiit/fotorenamer/TestConstants.java
+++ src/test/java/de/aikiit/fotorenamer/TestConstants.java
@@ -1,23 +1,24 @@
 /**
-Copyright 2011, Aiki IT, FotoRenamer
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright 2011, Aiki IT, FotoRenamer
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.aikiit.fotorenamer;
 
 import org.junit.Ignore;
 
 import java.io.File;
+import java.util.regex.Pattern;
 
 /**
  * This helper class contains relevant constants for testing.
@@ -27,27 +28,35 @@ import java.io.File;
  */
 @Ignore("Only constants defined here.")
 public class TestConstants {
+
+    /**
+     * This is the base name of the image.
+     */
+    public final static String PLAIN_FILE_NAME = "IMG_7559_mini.JPG";
+
     /**
      * Constant to fully qualified directory with all test images.
      */
-    public static final String FULLPATH_IMAGES =  System.getProperty("user.dir")
+    public static final String FULLPATH_IMAGES = System.getProperty("user.dir")
             + File.separator + "target" + File.separator + "test-classes"
             + File.separator + "data" + File.separator;
 
     /**
      * Constant to describe full qualified path to one test image.
      */
-    public static final String FULLPATH_TEST_IMG = FULLPATH_IMAGES + "IMG_7559_mini.JPG";
+    public static final String FULLPATH_TEST_IMG = FULLPATH_IMAGES + PLAIN_FILE_NAME;
 
     /**
      * Constant to full qualified picture url after processing.
      */
-    public static final String FULLPATH_TEST_IMG_RENAMED = FULLPATH_IMAGES + "20110130_131102_IMG_7559_mini.JPG";
+    public static final String FULLPATH_TEST_IMG_RENAMED = FULLPATH_IMAGES + "20110130_131102_" + PLAIN_FILE_NAME;
 
     /**
      * Name of the test image after renaming.
      */
     public final static String IMAGE_NAME_RENAMED =
-            "20110130_131102_IMG_7559_mini.JPG";
+            "20110130_131102_" + PLAIN_FILE_NAME;
+
+    public final static Pattern IS_TEST_FILE = Pattern.compile(".*(" + PLAIN_FILE_NAME + ").*", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
 
 }
--- src/test/java/de/aikiit/fotorenamer/image/MetaDataExtractorTest.java
+++ src/test/java/de/aikiit/fotorenamer/image/MetaDataExtractorTest.java
@@ -1,29 +1,38 @@
 /**
-Copyright 2011, Aiki IT, FotoRenamer
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright 2011, Aiki IT, FotoRenamer
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.aikiit.fotorenamer.image;
 
 import de.aikiit.fotorenamer.TestConstants;
 import org.apache.commons.imaging.formats.tiff.constants.ExifTagConstants;
 import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Read properties from test images and verify exif-metadata extraction.
@@ -33,26 +42,37 @@ import static org.junit.Assert.assertNull;
 public class MetaDataExtractorTest {
     static final private Logger LOG = Logger.getLogger(MetaDataExtractorTest.class);
 
+    @Test
+    public void makeSurePatternMatchesMultipleRenamedFiles() {
+        Matcher matcher = TestConstants.IS_TEST_FILE.matcher("123123123123_20110130_131102_IMG_7559_mini.JpG");
+        assertTrue(matcher.find());
+    }
+
     /**
      * Test metadata extraction from above example image.
+     *
      * @throws Exception in case of errors.
      */
     @Test
-    public final void readDirectoryContent() throws Exception {
+    public final void ensureFileIsRenamed() throws Exception {
         File f = new File(TestConstants.FULLPATH_TEST_IMG);
 
-        if(!f.exists()) {
+        if (!f.exists()) {
             LOG.debug("Switching to renamed file, since another test may have touched it before.");
             f = new File(TestConstants.FULLPATH_TEST_IMG_RENAMED);
         }
 
         LOG.debug("Extracting metadata from " + f.getAbsolutePath());
-        assertEquals(TestConstants.IMAGE_NAME_RENAMED, MetaDataExtractor
-                .generateCreationDateInCorrectFormat(f));
+
+        String renamedFile =
+                MetaDataExtractor
+                        .generateCreationDateInCorrectFormat(f);
+        assertTrue(renamedFile.endsWith(TestConstants.PLAIN_FILE_NAME));
     }
 
     /**
      * Checks assertion failure with null parameter.
+     *
      * @throws Exception in case of errors.
      */
     @Test(expected = AssertionError.class)
@@ -62,6 +82,7 @@ public class MetaDataExtractorTest {
 
     /**
      * Checks assertion failure with null parameter.
+     *
      * @throws Exception in case of errors.
      */
     @Test(expected = AssertionError.class)
