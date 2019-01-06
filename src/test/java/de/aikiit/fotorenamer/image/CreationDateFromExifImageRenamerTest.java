f12ca5b6ead5eee1242cf29034157ab7774ec428
Hugo Hirsch <github@hugo-hirsch.de>
2019-01-06 21:17:16 +0100

    Fix deprecated project imports

--- src/test/java/de/aikiit/fotorenamer/image/CreationDateFromExifImageRenamerTest.java
+++ src/test/java/de/aikiit/fotorenamer/image/CreationDateFromExifImageRenamerTest.java
@@ -23,9 +23,9 @@ import java.io.File;
 
 import static de.aikiit.fotorenamer.TestConstants.FULLPATH_IMAGES;
 import static de.aikiit.fotorenamer.TestConstants.FULLPATH_TEST_IMG;
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Test image renaming.
