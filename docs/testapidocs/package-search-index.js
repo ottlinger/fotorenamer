275bc3c1f9cfc4d4fcbd631d46015c37fc66590f
Hugo Hirsch <github@hugo-hirsch.de>
2020-08-05 14:27:20 +0200

    Update site via script

---
+++ docs/testapidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"de.aikiit.fotorenamer"},{"l":"de.aikiit.fotorenamer.exception"},{"l":"de.aikiit.fotorenamer.gui"},{"l":"de.aikiit.fotorenamer.image"},{"l":"de.aikiit.fotorenamer.util"}]
